95ec7378dabedad5338679fe2f5a847d029e1ddc
ananyadaniel <ananyadaniels@gmail.com>
2023-06-03 12:42:00 +0530

    settings changed

--- ActivityPoints/settings.py
+++ ActivityPoints/settings.py
@@ -48,7 +48,18 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_auth'
 ]
+AUTH_USER_MODEL = 'user_api.urls'
 
+REST_FRAMEWORK = {
+   'DEFAULT_PERMISSION_CLASSES': (
+       'rest_framework.permissions.IsAuthenticated',
+   ),
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+       'rest_framework.authentication.SessionAuthentication',
+       'rest_framework.authentication.BasicAuthentication',
+   ),
+}
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
